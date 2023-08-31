d300cc89b8de30604aeffab260260c88393d0ccb
Mike Tickle <mtickle@gmail.com>
2023-08-31 13:24:40 -0400

    lets go

--- routes/routes.js
+++ routes/routes.js
@@ -45,7 +45,7 @@ router.get("/getAllFlights", auth.checkKey, async (req, res) => {
   const recordLimit = req.query.limit || 10
 
   try {
-    const data = await flightsViewModel.find().limit(recordLimit).sort({ dateArr: -1, timeArr: -1 });
+    const data = await flightsModel.find().limit(recordLimit).sort({ dateArr: -1, timeArr: -1 });
     res.json(data);
   } catch (error) {
     res.status(500).json({ message: error.message });
