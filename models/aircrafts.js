7da72526379943f8144dd7da263de83bcff2c553
Mike Tickle <mtickle@gmail.com>
2024-07-10 09:24:19 -0400

    help

--- models/aircrafts.js
+++ models/aircrafts.js
@@ -1,7 +1,59 @@
 import mongoose, { Schema } from "mongoose";
 
 const aircraftsSchema = new Schema({
-  n_number: {
+  icao24 : {
+    required: true,
+    type: String,
+  },
+  timestamp : {
+    required: true,
+    type: String,
+  },
+  acars: {
+    required: true,
+    type: String,
+  },
+  adsb: {
+    required: true,
+    type: String,
+  },
+  built: {
+    required: true,
+    type: String,
+  },
+  country : {
+    required: true,
+    type: String,
+  },
+  engines : {
+    required: true,
+    type: String,
+  },
+  icaoAircraftClass : {
+    required: true,
+    type: String,
+  },
+  manufacturerIcao : {
+    required: true,
+    type: String,
+  },
+  manufacturerName : {
+    required: true,
+    type: String,
+  },
+  model : {
+    required: true,
+    type: String,
+  },
+  modes : {
+    required: true,
+    type: String,
+  },
+  operatorCallsign : {
+    required: true,
+    type: String,
+  },
+  operatorIcao : {
     required: true,
     type: String,
   },
