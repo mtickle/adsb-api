5c5d006ea41172ced285424723752e37465389af
Mike Tickle <mtickle@gmail.com>
2024-07-27 07:29:47 -0400

    mergedFlightHistory

---
+++ config/mergedFlightHistory.js
@@ -0,0 +1,37 @@
+[
+  {
+    $addFields: {
+      hex_code_lower: {
+        $toLower: "$hex_code"
+      }
+    }
+  },
+   {
+      $lookup: {
+        from: "aircrafts",
+        localField:  "hex_code_lower",
+        foreignField: "icao24",
+        as: "ref",
+      },
+    },
+    {
+      $unwind: "$ref",
+    },
+   {
+      $project: {
+        _id: 1,
+        hex_code: "$hex_code_lower",
+        flight: 1,
+        flight_time: 1,
+        model: "$ref.model",
+        operator: "$ref.operator",
+        registration: "$ref.registration",
+        manufacturerName: "$ref.manufacturerName",
+        operatorCallsign: "$ref.operatorCallsign",
+        owner: "$ref.owner"
+      },
+    },
+   {
+         $out: "mergedFlightHistory",
+     },
+]
